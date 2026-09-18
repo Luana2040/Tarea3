6cc2d5cb3b20137494769954ffbcd92b25548f8c
fernandezpinedojhonatan <fernandezpinedojhonatan@gmail.com>
2026-09-18 11:41:35 -0500

    subiendo el primer commit

--- app/src/main/java/com/example/mi_clima/ClimaResponse.kt
+++ app/src/main/java/com/example/mi_clima/ClimaResponse.kt
@@ -1,2 +1,17 @@
 package com.example.mi_clima
 
+data class ClimaResponse(
+    val latitude: Double,
+    val longitude: Double,
+    val current: Current
+)
+
+data class Current(
+    val time: String,
+    val temperature_2m: Double,
+    val relative_humidity_2m: Int,
+    val apparent_temperature: Double,
+    val precipitation: Double,
+    val weather_code: Int,
+    val wind_speed_10m: Double
+)
